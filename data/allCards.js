de7bf6b58ec09cd8e02c6524a3035189892c8869
carla <carlacfw@gmail.com>
2017-07-30 16:31:02 +1200

    database done

---
+++ data/allCards.js
@@ -0,0 +1,66 @@
+const allCards = {
+  cards: [
+    {
+      id: 1,
+      matchId: 1,
+      text: "==="
+    },
+    {
+      id: 2,
+      matchId: 1,
+      text: "strict equal to"
+    },
+    {
+      id: 3,
+      matchId: 2,
+      text: "? :"
+    },
+    {
+      id: 4,
+      matchId: 2,
+      text: "shortcut for if statement"
+    },
+    {
+      id: 5,
+      matchId: 3,
+      text: "="
+    },
+    {
+      id: 6,
+      matchId: 3,
+      text: "used to assign a value"
+    },
+    {
+      id: 7,
+      matchId: 4,
+      text: "!=="
+    },
+    {
+      id: 8,
+      matchId: 4,
+      text: "not equal to"
+    },
+    {
+      id: 9,
+      matchId: 5,
+      text: "var"
+    },
+    {
+      id: 10,
+      matchId: 5,
+      text: "give it a name and assing it to a value"
+    },
+    {
+      id: 11,
+      matchId: 6,
+      text: "''"
+    },
+    {
+      id: 12,
+      matchId: 6,
+      text: "use it around a word to create a string"
+    }
+  ]
+
+
+export default allCards
