1412891b38dd4820ff99e9a2d7efc6c5bae15d6d
carla <carlacfw@gmail.com>
2017-07-30 17:35:19 +1200

    rules component and route working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import {HashRouter as Router, Route} from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import Rules from './Rules'
 
 const App = () => {
   return (
+    <div className='home-page'>
     <h1>JS Memory Game</h1>
+    <Router>
+      <div className='routes'>
+        <Route exact path='/' component={Rules}/>
+      </div>
+
+    </Router>
+    </div>
   )
 }
 
---
+++ client/components/Rules.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {HashRouter as Router, Route} from 'react-router-dom'
+import {Link} from 'react-router-dom'
+
+
+
+export default function (props) {
+  return (
+    <div className="rules">
+      <h3>Are you ready for testing your knowledge?</h3>
+      <p></p>
+
+    </div>
+  )
+}
