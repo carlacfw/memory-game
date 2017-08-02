01334fd9310032f70d227975c7219b0062018729
todddrinkwater <daniel-reason>
2017-08-03 11:44:14 +1200

    need to finish functions, to check cards, check matchIds and clean temparray

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -5,28 +5,90 @@ import {Link} from 'react-router-dom'
 import allCards from '../../data/allCards'
 import Card from './Card'
 
-
-export default function (props) {
-  function shuffle(input) {
-  for (var i = input.length-1; i >=0; i--) {
-    var randomIndex = Math.floor(Math.random()*(i+1))
-    var itemAtIndex = input[randomIndex]
-
-    input[randomIndex] = input[i]
-    input[i] = itemAtIndex
+export default class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      cards: this.shuffle([...allCards.cards]),
+      revealed: [],
+      tempRevealed: []
+    }
   }
-  return input
+  shuffle(input) {
+    for (var i = input.length-1; i >=0; i--) {
+      var randomIndex = Math.floor(Math.random()*(i+1))
+      var itemAtIndex = input[randomIndex]
+      input[randomIndex] = input[i]
+      input[i] = itemAtIndex
+    }
+    return input
+  }
+  checkMatch(card1, card2) {
+    //if match_ids are equal, return true
+    return false
+  }
+  checkTwoTemporaryCards(tempRevealed) {
+    let {revealed} = this.state
+    tempRevealed.length == 2
+      return tempRevealed
+
+
+    //check if there are two
+
+    // tempRevealed = [] //only do this if there are two
+    this.setState({tempRevealed, revealed})
+  }
+  clickCard(card) {
+    console.log(card);
+    let {tempRevealed} = this.state
+    tempRevealed.push(card)
+    if(tempRevealed.length == 2){
+      return true
+    } //check if there are two temporary cards
+    this.checkTwoTemporaryCards(tempRevealed)
+    this.setState({tempRevealed})
+  }
+  checkRevealed(card) {
+    let {revealed, tempRevealed} = this.state
+    // console.log(revealed.indexOf(card) != -1 || tempRevealed.indexOf(card) != -1);
+    return (revealed.indexOf(card) != -1 || tempRevealed.indexOf(card) != -1)
+  }
+  render() {
+    let {cards, revealed, tempRevealed} = this.state
+    return (
+      <div className="board">
+        {cards.map((card) => {
+          let isRevealed = this.checkRevealed(card)
+          console.log({isRevealed});
+          return (<Card isRevealed={isRevealed} card={card} clickCard={this.clickCard.bind(this)}/>)
+        })}
+
+      </div>
+    )
+  }
 }
 
+// export default function (props) {
+//   function shuffle(input) {
+//   for (var i = input.length-1; i >=0; i--) {
+//     var randomIndex = Math.floor(Math.random()*(i+1))
+//     var itemAtIndex = input[randomIndex]
+//
+//     input[randomIndex] = input[i]
+//     input[i] = itemAtIndex
+//   }
+//   return input
+// }
 
-  let cards = shuffle(allCards.cards)
-  console.log(cards)
-  return (
-    <div className="board">
-      {cards.map((card) => {
-        return (<Card data={card}/>)
-      })}
-
-    </div>
-  )
-}
+//
+//   let cards = shuffle([...allCards.cards])
+//   console.log(cards)
+//   return (
+//     <div className="board">
+//       {cards.map((card) => {
+//         return (<Card data={card}/>)
+//       })}
+//
+//     </div>
+//   )
+// }
--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -2,25 +2,34 @@ import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
 import {Link} from 'react-router-dom'
 
-export default class Card extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isUncovered: false
-    }
-  }
-  toggleCover() {
-    this.setState({
-      isUncovered: !this.state.isUncovered
-    })
-  }
-
-  render() {
-    return (
-      <div className="card" onClick={this.toggleCover.bind(this)}>
-        {this.state.isUncovered && <p>{this.props.data.text}</p>}
-
-      </div>
-    )
-  }
+export default function Card ({isRevealed, card, clickCard}) {
+  console.log({isRevealed,card});
+  return (
+    <div className="card" onClick={() => clickCard(card)}>
+      {isRevealed && <p>{card.text}</p>}
+    </div>
+  )
 }
+
+// export default class Card extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       isUncovered: false
+//     }
+//   }
+//   toggleCover() {
+//     this.setState({
+//       isUncovered: !this.state.isUncovered
+//     })
+//   }
+//
+//   render() {
+//     return (
+//       <div className="card" onClick={this.toggleCover.bind(this)}>
+//         {this.state.isUncovered && <p>{this.props.data.text}</p>}
+//
+//       </div>
+//     )
+//   }
+// }
