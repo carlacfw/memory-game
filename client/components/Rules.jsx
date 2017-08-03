2f6b5324bc306dc50ab49b6c9136702eba34e0ca
todddrinkwater <daniel-reason>
2017-08-03 16:59:30 +1200

    the game is working

--- client/components/Rules.jsx
+++ client/components/Rules.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
 import {Link} from 'react-router-dom'
 
-
-
-export default function (props) {
+export default function(props) {
   return (
     <div className="rules">
       <h3>Are you ready for testing your knowledge?</h3>
       <p></p>
-<Link to='/board'>let's play</Link>
+      <Link to='/board'>let's play</Link>
     </div>
   )
 }
