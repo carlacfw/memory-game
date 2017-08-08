9ea374912a629716bd4815d960b67b857849b667
carla <carlacfw@gmail.com>
2017-08-08 13:02:04 +1200

    game is ready - button is not working

--- client/components/Rules.jsx
+++ client/components/Rules.jsx
@@ -5,9 +5,14 @@ import {Link} from 'react-router-dom'
 export default function(props) {
   return (
     <div className="rules">
+      <h3>How the game works</h3>
+      <p>The game has a time limit of 60 seconds</p>
+      <p>The goal is matching the JavaScript symbols with their descriptions</p>
+      <p>It will be easier if you have some basic knowledge in JavaScrip and it's symbols</p>
       <h3>Are you ready for testing your knowledge?</h3>
-      <p></p>
-      <Link to='/board'>let's play</Link>
+      <div className='play'>
+        <Link to='/board'>let's play</Link>
+      </div>
     </div>
-  )
+    )
 }
