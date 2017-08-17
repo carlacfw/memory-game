985508bb6aab6bea7caca0b04e7ed657688d987a
Joshua <joshuafoggnz@gmail.com>
2017-08-17 18:58:07 +1200

    correct JS

--- client/components/Rules.jsx
+++ client/components/Rules.jsx
@@ -8,7 +8,7 @@ export default function(props) {
       <h3>How the game works</h3>
       <p>The game has a time limit of 60 seconds</p>
       <p>The goal is matching the JavaScript symbols with their descriptions</p>
-      <p>It will be easier if you have some basic knowledge in JavaScrip and it's symbols</p>
+      <p>It will be easier if you have some basic knowledge in JavaScript and it's symbols</p>
       <h3>Are you ready for testing your knowledge?</h3>
       <div className='play'>
         <Link to='/board'>let's play</Link>
